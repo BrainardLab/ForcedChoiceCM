2eb4c069a921b8b7f6072debf89806ddf59e33e7
David Brainard <brainard@psych.upenn.edu>
2019-08-13 17:21:25 -0400

    Stimulus filtering routine, to keep the size managable.

--- toolbox/QuestPlus/qpFCCMStimDomainCheck.m
+++ toolbox/QuestPlus/qpFCCMStimDomainCheck.m
@@ -1,8 +1,8 @@
-function paramsOK = qpFCCMStimDomainCheck(stimParamsVec,type)
+function paramsOK = qpFCCMStimDomainCheck(stimParamsVec,type,stimParamsStruct)
 % Stimulus parameter check for force-choice color matching
 %
 % Usage:
-%     paramsOK = qpFCCMStimDomainCheck(stimParams,type)
+%     paramsOK = qpFCCMStimDomainCheck(stimParamsVec,type,stimParamsStruct)
 %
 % Description:
 %     Check whether passed parameters are valid stimuli for forcec choice
@@ -20,18 +20,55 @@ function paramsOK = qpFCCMStimDomainCheck(stimParamsVec,type)
 %% Assume ok
 paramsOK = true;
 
-%% Check that concentration is non-negative
-if (psiParams(1) < 0)
-    paramsOK = false;
+switch (type)
+    case 'basic'
+        [testParams,comparison1Opponent,comparison2Opponent] = StimulusVecToParams(type,stimParamsVec,stimParamsStruct);
+        
+        % Only go in one opponent direction in each comparison
+        index1 = find(comparison1Opponent == 0);
+        if (length(index1) < 2)
+            paramsOK = false; return;
+        end
+        index2 = find(comparison2Opponent == 0);
+        if (length(index2) < 2)
+            paramsOK = false; return;
+        end
+        
+        index1 = find(comparison1Opponent ~= 0);
+        index2 = find(comparison2Opponent ~= 0);
+        if (~isempty(index1))
+            if (~isempty(index2))
+                if (index1 ~= index2)
+                    paramsOK = false; return;
+                end
+            end
+        end
+        
+        % Two comparisons should be in same direction
+        if (comparison1Opponent(index1) ~= 0 & comparison1Opponent(index1) ~= 0)
+            if (sign(comparison1Opponent(index1)) ~= sign(comparison2Opponent(index2)))
+                paramsOK = false; return;
+            end
+        end
+        
+        % Don't run trials when two comparisons are same stimulus
+        %
+        % This checks for the case where all entries are zero and thus
+        % the same.
+        if (isempty(index1) & isempty(index2))
+            paramsOK = false; return;
+        end
+        
+        % If both are non-zero, then check if they are the same.
+        if (~isempty(index1) & ~isempty(index2))
+            if (comparison1Opponent(index1) == comparison2Opponent(index2))
+                paramsOK = false; return;
+            end
+        end
+        
+    otherwise
+        error('Unknown parameter vector type requested');
+        
 end
 
-%% Check whether boundary parameters are OK
-%
-% This is signaled by returning NaN when the boundaries are
-% not in increasing order.
-[boundaries,sortIndex] = sort(psiParams(2:end),'ascend');
-nOutcomes = length(boundaries);
-if (any(sortIndex ~= 1:nOutcomes))
-    paramsOK = false;
-end
 
