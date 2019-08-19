d99ad4ebc5347229a9d150601ca1d72ec9622e42
David Brainard <brainard@psych.upenn.edu>
2019-08-19 15:31:18 -0400

    Starting to experiment with how well it might all work.

--- tutorials/t_QuestPlusSimulation.m
+++ tutorials/t_QuestPlusSimulation.m
@@ -27,6 +27,7 @@ stimParamsStruct.testParams = DefaultTestParams('monochromatic',S);
 psiParamsStruct.coneParams = DefaultConeParams('cie_asano');
 psiParamsStruct.colorDiffParams = DefaultColorDiffParams('opponentContrast');
 psiParamsStructRef = psiParamsStruct;
+referencePsiParamsVec = ObserverParamsToVec('basic',psiParamsStructRef);
 
 %% Set an adaptation spectrum
 adaptationSpd = stimParamsStruct.matchApparatusParams.primaryBasis*[0.25 0.25 0.25]';
@@ -37,10 +38,10 @@ psiVecType = 'basic';
 
 %% Set up simulated observer parameters
 simulatedPsiParamsStruct = psiParamsStruct;
-simulatedPsiParamsStruct.coneParams.indDiffParams.dlens = 0;
+simulatedPsiParamsStruct.coneParams.indDiffParams.dlens = -10;
 simulatedPsiParamsStruct.coneParams.indDiffParams.dmac = 0;
 simulatedPsiParamsStruct.coneParams.indDiffParams.dphotopigment = [0 0 0]';
-simulatedPsiParamsStruct.coneParams.indDiffParams.lambdaMaxShift = [-2.6 2.3 0]';
+simulatedPsiParamsStruct.coneParams.indDiffParams.lambdaMaxShift = [-2.6 2.3 0.5]';
 simulatedPsiParamsStruct.colorDiffParams.noiseSd = 0.03;
 simulatedPsiParamsVec = ObserverParamsToVec(psiVecType,simulatedPsiParamsStruct);
 
@@ -75,11 +76,19 @@ USE_PRECOMPUTE = false;
 if (~USE_PRECOMPUTE)
     fprintf('Initializing quest structure ...\n');
     startTime = GetSecs;
+%     questDataRaw = qpInitialize(...
+%         'nOutcomes', 2, ...
+%         'qpPF',qpPFFun, ...
+%         'stimParamsDomainList',{440:20:680, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06}, ...
+%         'psiParamsDomainList',{-30:15:30, -40:20:40, -20:20:20, -20:20:20, -20:20:20, -4:2:4, -4:2:4, -2:2:2, 0.02}, ...
+%         'filterStimParamsDomainFun',@(stimParamsVec) qpFCCMStimDomainCheck(stimParamsVec,stimVecType,stimParamsStruct), ...
+%         'verbose', true ...
+%         );
     questDataRaw = qpInitialize(...
         'nOutcomes', 2, ...
         'qpPF',qpPFFun, ...
-        'stimParamsDomainList',{440:20:680, -0.06:0.03:0.06, -0.06:0.03:0.06, -0.06:0.03:0.06, -0.06:0.03:0.06, -0.06:0.03:0.06, -0.06:0.03:0.06}, ...
-        'psiParamsDomainList',{-30:15:30, -40:20:40, -20:20:20, -20:20:20, -20:20:20, -4:2:4, -4:2:4, -2:2:2, 0.02}, ...
+        'stimParamsDomainList',{440:20:680, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06, -0.06:0.06:0.06}, ...
+        'psiParamsDomainList',{-30:15:30, 0, 0, 0, 0, -4:2:4, -4:2:4, -2:2:2, 0.02}, ...
         'filterStimParamsDomainFun',@(stimParamsVec) qpFCCMStimDomainCheck(stimParamsVec,stimVecType,stimParamsStruct), ...
         'verbose', true ...
         );
@@ -95,7 +104,7 @@ end
 %% qpRun estimating the parameters
 fprintf('*** Simluated run, estimate parametric cone fundamentals:\n');
 rng('default'); rng(3008,'twister');
-nTrials = 256;
+nTrials = 1024;
 startTime = GetSecs;
 questData = questDataRaw;
 for tt = 1:nTrials
@@ -131,15 +140,69 @@ fprintf('Max posterior QUEST+ parameters: %0.1f, %0.1f, %0.1f, %0.1f. %0.1f, %0.
 % Use psiParams from QUEST+ as the starting parameter for the search, and
 % impose as parameter bounds the range provided to QUEST+.
 psiParamsFit = qpFit(questData.trialData,questData.qpPF,psiParamsQuest,questData.nOutcomes,...
-     'lowerBounds', [0, 0, 0, 0, 0, -4, -4, 0, 0.001],'upperBounds',[0, 0, 0, 0, 0, 4, 4, 0, 0.06]);
-fprintf('Maximum likelihood fit parameters:  %0.1f, %0.1f, %0.1f, %0.1f. %0.1f, %0.1f, %0.1f, %0.1f, %0.3f\n', ...
+     'lowerBounds', [-30, -0, 0, 0, 0, -4, -4, -2, 0.001],'upperBounds',[30, 0, 0, 0, 0, 4, 4, 2, 0.06]);
+fprintf('Maximum likelihood fit parameters:  %0.3f, %0.3f, %0.3f, %0.3f. %0.3f, %0.3f, %0.3f, %0.3f, %0.3f\n', ...
     psiParamsFit(1),psiParamsFit(2),psiParamsFit(3),psiParamsFit(4), ...
     psiParamsFit(5),psiParamsFit(6),psiParamsFit(7),psiParamsFit(8),psiParamsFit(9));
 % psiParamsCheck = [-359123 -485262 11325 0];
 % assert(all(psiParamsCheck == round(10000*psiParamsFit)),'No longer get same ML estimate for this case');
-disp
 
- 
+%% Evaluate fits a little bit
+%
+% Get stimulus counts
+stimCounts = qpCounts(qpData(questData.trialData),questData.nOutcomes);
+
+% Log likelihoods
+logLikelihoodRef = -qpFitError(referencePsiParamsVec,stimCounts,questData.qpPF);
+logLikelihoodSimulated = -qpFitError(simulatedPsiParamsVec,stimCounts,questData.qpPF);
+logLikelihoodQuest = -qpFitError(psiParamsQuest,stimCounts,questData.qpPF);
+logLikelihoodFit = -qpFitError(psiParamsFit,stimCounts,questData.qpPF);
+fprintf('Log likelihoods:\n');
+fprintf('\tReference fundamentals: %0.2f\n',logLikelihoodRef);
+fprintf('\tQuest max posterior:    %0.2f\n',logLikelihoodQuest);
+fprintf('\tSimulated fundamentas:  %0.2f\n',logLikelihoodSimulated);
+fprintf('\tMax likelihood fit:     %0.2f\n',logLikelihoodFit);
+
+% Compute fundamentals
+simulatedParamsStruct = ObserverVecToParams('basic',simulatedPsiParamsVec,psiParamsStruct);
+questParamsStruct = ObserverVecToParams('basic',psiParamsQuest,psiParamsStruct);
+fitParamsStruct = ObserverVecToParams('basic',psiParamsFit,psiParamsStruct);
+T_simulated = ComputeObserverFundamentals(simulatedParamsStruct.coneParams,S);
+T_quest = ComputeObserverFundamentals(questParamsStruct.coneParams,S);
+T_fit = ComputeObserverFundamentals(fitParamsStruct.coneParams,S);
+
+fundamentalsFig = figure; clf;
+set(gcf,'Position',[50 420 2400 900]);
+subplot(1,3,1); hold on
+plot(SToWls(S),TRef(1,:),'k','LineWidth',3);
+plot(SToWls(S),T_simulated(1,:),'r','LineWidth',3);
+plot(SToWls(S),T_quest(1,:),'b:','LineWidth',2);
+plot(SToWls(S),T_fit(1,:),'b','LineWidth',2);
+xlabel('Wavelength (nm)');
+ylabel('Fundamental');
+title('L cone');
+legend({'Reference', 'Simulated','Quest Max','Fit'});
+
+subplot(1,3,2); hold on
+plot(SToWls(S),TRef(2,:),'k','LineWidth',3);
+plot(SToWls(S),T_simulated(2,:),'r','LineWidth',3);
+plot(SToWls(S),T_quest(2,:),'b:','LineWidth',2);
+plot(SToWls(S),T_fit(2,:),'b','LineWidth',2);
+xlabel('Wavelength (nm)');
+ylabel('Fundamental');
+title('M cone');
+legend({'Reference', 'Simulated','Quest Max','Fit'});
+
+subplot(1,3,3); hold on
+plot(SToWls(S),TRef(3,:),'k','LineWidth',3);
+plot(SToWls(S),T_simulated(3,:),'r','LineWidth',3);
+plot(SToWls(S),T_quest(3,:),'b:','LineWidth',2);
+plot(SToWls(S),T_fit(3,:),'b','LineWidth',2);
+xlabel('Wavelength (nm)');
+ylabel('Fundamental');
+title('S cone');
+legend({'Reference', 'Simulated','Quest Max','Fit'});
+
 % Plot trial locations together with maximum likelihood fit.
 %
 % Point transparancy visualizes number of trials (more opaque -> more
