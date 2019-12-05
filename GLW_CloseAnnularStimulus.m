01ca42c8b1e6eb1f75af2402b5dd33eff08472dc
Deena Elul <delul@sas.upenn.edu>
2019-12-05 15:38:46 -0500

    program to close annular stimulus window

---
+++ GLW_CloseAnnularStimulus.m
@@ -0,0 +1,14 @@
+try
+    win = GLWindow('windowID', length(mglDescribeDisplays));
+    win.close(); 
+catch e 
+        disp('An exception was raised');
+    
+    % Close the window if it was succesfully created.
+    if ~isempty(win)
+        win.close;
+    end
+    
+    % Send the error back to the Matlab command window.
+    rethrow(e);
+end 
