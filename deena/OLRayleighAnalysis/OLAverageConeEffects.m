a53f1e0a5cc6c28098cdc44d41b401d599dd2802
Deena Elul <delul@sas.upenn.edu>
2020-01-24 10:47:18 -0500

    edits to analysis program

--- deena/OLRayleighAnalysis/OLAverageConeEffects.m
+++ deena/OLRayleighAnalysis/OLAverageConeEffects.m
@@ -11,7 +11,7 @@ p.parse(varargin{:});
 % Arrays for storing aggregate data. These will have three columns for L,
 % M, and S cones. 
 primaryCones = []; 
-testCones = []; 
+testCones = [];  
 
 % Find files for subjects
 baseDir= fullfile(getpref('ForcedChoiceCM','rayleighDataDir'),subjectID, subjectID);
@@ -25,18 +25,25 @@ end
 [row, ~] = size(files); 
 for i = 1:row
     fName = fullfile(files(i).folder, files(i).name);
+    % Skip over measurement files if testing non-measured data
+    if (all(fName(end-7:end-4) == 'meas')) && ~p.Results.measured
+        continue; 
+    end 
     [pCones, tCones] = OLGetConeEffects(fName); 
-    primaryCones = [primaryCones; pCones]; 
-    testCones = [testCones; tCones]; 
+    primaryCones = [primaryCones; pCones']; 
+    testCones = [testCones; tCones']; 
 end 
 
 % Average primary and test cone responses 
 [numMatches, ~] = size(primaryCones); 
-[pLSum, pMSum, pSSum] = sum(primaryCones, 1); 
-[tLSum, tMSum, tSSum] = sum(testCones, 1); 
 
-primaryAverages = [pLSum, pMSum, pSSum] / numMatches; 
-testAverages = [tLSum, tMSum, tSSum] / numMatches; 
+primaryAverages = mean(primaryCones, 1); 
+primarySD = std(primaryCones, 1); 
 
-OLPlotConeEffects(primaryAverages, testAverages, subjectID, 0, 'average', true, 'err', [tktktk]); 
+testAverages = mean(testCones, 1); 
+testSD = std(primaryCones, 1); 
+
+err = [primarySD(1), testSD(1), primarySD(2), testSD(2), primarySD(3), testSD(3)];
+
+OLPlotConeEffects(primaryAverages, testAverages, subjectID, 0, 'average', true, 'err', err); 
 end
