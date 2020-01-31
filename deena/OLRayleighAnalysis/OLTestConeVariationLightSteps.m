2e68b994e86129fc27baaf53c617e41d8cb327f0
Deena Elul <delul@sas.upenn.edu>
2020-01-31 09:59:44 -0500

    edits

---
+++ deena/OLRayleighAnalysis/OLTestConeVariationLightSteps.m
@@ -0,0 +1,1 @@
+lightData = load('/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/ForcedChoiceCM/OLRayleighMatch/precomputedStartStops/OLRayleighMatchDefaultSpectralSettings.mat'); 
