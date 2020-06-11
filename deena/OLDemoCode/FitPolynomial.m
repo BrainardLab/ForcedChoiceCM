e03ec6bdc31fa93c5b0d17f7cfd76b09d464d24b
deena <delul@sas.upenn.edu>
2020-06-11 16:42:06 -0400

    fitparabola -> fitpolynomial

---
+++ deena/OLDemoCode/FitPolynomial.m
@@ -0,0 +1,17 @@
+function [error, yPred] = FitPolynomial(params, xVals,yObs)
+% Use with fmincon to find the error associated with a given polynomial fit
+
+% Find the predicted y values
+yPred = zeros(yObs, 1); 
+degree = length(params); 
+for i = 1:degree
+    yPred = yPred + params(i).*xVals.^(degree-i); 
+end
+
+% Calculate the root mean square error
+squaredDiffs = zeros(1,length(yPred));
+for j = 1:length(yPred)
+    squaredDiffs(j) = (yObs(j) - yPred(j))^2;  
+end 
+error = sqrt(mean(squaredDiffs));
+end 
